a143e7507208ee872aaa64b5f6edd6811dd8f9b4
s8140156 <s8140156@gmail.com>
2024-01-14 19:05:21 +0800

    修正最新消息彈出視窗看不到顯示(因為上層沒有設定relative)成功完成圖片輪播與限制出現張數

--- index.php
+++ index.php
@@ -30,6 +30,7 @@ if(isset($_GET['error'])){
   <link href="./css/glightbox.min.css" rel="stylesheet">
   <link href="./css/remixicon.css" rel="stylesheet">
   <link href="./css/swiper-bundle.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
   <!-- Template Main CSS File -->
   <link href="./css/styleF.css" rel="stylesheet">
@@ -61,11 +62,11 @@ if(isset($_GET['error'])){
 						?>
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="active" href="index.html">Home</a></li>
-          <li><a href="our-story.html">Our Story</a></li>
-          <li><a href="events.html">Events</a></li>
-          <li><a href="gallery.html">Gallery</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+          <li><a class="active" href="index.php">Home</a></li>
+          <li><a href="#p-1">小道消息</a></li>
+          <li><a href="#p-2">實用小工具</a></li>
+          <li><a href="#p-3">我的這一班</a></li>
+          <!-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="#">Drop Down 1</a></li>
               <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
@@ -81,17 +82,17 @@ if(isset($_GET['error'])){
               <li><a href="#">Drop Down 3</a></li>
               <li><a href="#">Drop Down 4</a></li>
             </ul>
-          </li>
+          </li> -->
           <!-- <li><a href="contact.html">Contact</a></li> -->
           <?php
 					if(isset($_SESSION['login'])){
           ?>
           <li>
-            <button class="btn btn-success rounded-pill px-3" onclick="location.href='back.php'">返回管理</button>
+            <button class="btn btn-success rounded-pill px-3 ms-5" onclick="location.href='back.php'">返回管理</button>
             <?php
 					  }else{
 	          ?>
-            <button class="btn btn-success rounded-pill px-3" onclick="location.href='login.php'">管理登入</button>
+            <button class="btn btn-success rounded-pill px-3 ms-5" onclick="location.href='login.php'">管理登入</button>
           </li>
             <?php
 				    }
@@ -140,8 +141,10 @@ if(isset($_GET['error'])){
   <main id="main">
 
     <!-- ======= My & Family Section ======= -->
+    <div id="p-1"></div>
     <section id="about" class="about">
       <div class="container">
+
 
         <div class="section-title">
           <h2>歡迎來到<?=$slogan['text'];?></h2>
@@ -161,7 +164,7 @@ if(isset($_GET['error'])){
           <div class="col-lg-6" id="mwww" loop="true"> <!--這邊是魔幻動圖-->
             <img src="./img/about.jpg" class="img-fluid" alt="">
           </div>
-          <div class="col-lg-6 pt-4 pt-lg-0">
+          <div class="col-lg-6 pt-4 pt-lg-0" style="position:relative">
             <!-- <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
               magna aliqua.
@@ -202,64 +205,86 @@ if(isset($_GET['error'])){
     </section><!-- End My & Family Section -->
 
     <!-- ======= Features Section ======= -->
+    <div id="p-2"></div>
     <section id="features" class="features">
       <div class="container">
 
+
+      <div class="section-title">
+          <h2>實用小工具</h2>
+        </div>
+
         <div class="row">
           <div class="col-lg-4 col-md-6 icon-box">
-            <div class="icon"><i class="bi bi-laptop"></i></div>
-            <h4 class="title"><a href="">Lorem Ipsum</a></h4>
-            <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+            <div class="icon"><i class="fa-solid fa-utensils"></i></div>
+            <h4 class="title"><a href="http://dinbendon.kento520.tw/">你餓了嗎？</a></h4>
+            <p class="description">要訂便當的同學，請在10:00前將錢交給值日生，最好是都給我零錢，謝謝</p>
           </div>
           <div class="col-lg-4 col-md-6 icon-box">
-            <div class="icon"><i class="bi bi-bar-chart"></i></div>
-            <h4 class="title"><a href="">Dolor Sitema</a></h4>
-            <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+            <div class="icon"><i class="fa-solid fa-school"></i></div>
+            <h4 class="title"><a href="https://stustkyhkm.wda.gov.tw/">上課遲到了嗎？</a></h4>
+            <p class="description">走過路過，不要錯過！看到異色的寶可夢立馬停下來抓，遲到了別忘了請假~還是請大家上下課注意路上安全喔！</p>
           </div>
           <div class="col-lg-4 col-md-6 icon-box">
-            <div class="icon"><i class="bi bi-bounding-box"></i></div>
-            <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
-            <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+            <div class="icon"><i class="fa-solid fa-calendar-days"></i></div>
+            <h4 class="title"><a href="https://wda.mackliu.com/schedule/149632">什麼？下週有3堂PHP的課？</a></h4>
+            <p class="description">隨時掌握何時會看到劉老師(?)請注意</p>
           </div>
           <div class="col-lg-4 col-md-6 icon-box">
-            <div class="icon"><i class="bi bi-broadcast"></i></div>
-            <h4 class="title"><a href="">Magni Dolores</a></h4>
-            <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            <div class="icon"><i class="fa-solid fa-person-chalkboard"></i></div>
+            <h4 class="title"><a href="https://mackliu.github.io/php-book/">月光林地還是月光寶盒？</a></h4>
+            <p class="description">文藝青年嘔心瀝血之作，管他標題是不是跟月光族有關係，取字串要用什麼函式？我上課都講過了喔！</p>
           </div>
           <div class="col-lg-4 col-md-6 icon-box">
-            <div class="icon"><i class="bi bi-camera"></i></div>
-            <h4 class="title"><a href="">Nemo Enim</a></h4>
-            <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+            <div class="icon"><i class="fa-solid fa-person-digging"></i></div>
+            <h4 class="title"><a href="">under construction</a></h4>
+            <p class="description">To be contined...</p>
           </div>
           <div class="col-lg-4 col-md-6 icon-box">
-            <div class="icon"><i class="bi bi-diagram-3"></i></div>
-            <h4 class="title"><a href="">Eiusmod Tempor</a></h4>
-            <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
+            <div class="icon"><i class="fa-solid fa-screwdriver-wrench"></i></div>
+            <h4 class="title"><a href="">under maintenance</a></h4>
+            <p class="description">Coming soon...</p>
           </div>
         </div>
+        <!-- <i class="fa-solid fa-triangle-exclamation"></i> --> <!--注意sign-->
+        <!-- <i class="fa-solid fa-person-digging"></i> --> <!--施工-->
+        <!-- <i class="fa-solid fa-screwdriver-wrench"></i> --> <!--維修-->
+
 
       </div>
     </section><!-- End Features Section -->
 
     <!-- ======= Recent Photos Section ======= -->
+    <div id="p-3"></div>
     <section id="recent-photos" class="recent-photos">
       <div class="container">
 
+
         <div class="section-title">
-          <h2>Recent Photos</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+          <h2>我們這一班</h2>
+          <p>目標是把班上22人都拍大頭照</p>
         </div>
 
         <div class="recent-photos-slider swiper">
           <div class="swiper-wrapper align-items-center">
-            <div class="swiper-slide"><a href="./img/recent-photos-1.jpg" class="glightbox"><img src="./img/recent-photos-1.jpg" class="img-fluid" alt=""></a></div>
-            <div class="swiper-slide"><a href="./img/recent-photos-2.jpg" class="glightbox"><img src="./img/recent-photos-2.jpg" class="img-fluid" alt=""></a></div>
+          <?php
+						$imgs = $Image->all(['sh' => 1]," limit 8");
+						
+	
+						foreach ($imgs as $idx => $img) {
+							// echo $idx;
+						?>
+            <div class="swiper-slide"><a href="./img/<?=$img['img'];?>" class="glightbox"><img src="./img/<?=$img['img'];?>" class="img-fluid" alt=""></a></div>
+            <?php
+						}
+						?>
+            <!-- <div class="swiper-slide"><a href="./img/recent-photos-2.jpg" class="glightbox"><img src="./img/recent-photos-2.jpg" class="img-fluid" alt=""></a></div>
             <div class="swiper-slide"><a href="./img/recent-photos-3.jpg" class="glightbox"><img src="./img/recent-photos-3.jpg" class="img-fluid" alt=""></a></div>
             <div class="swiper-slide"><a href="./img/recent-photos-4.jpg" class="glightbox"><img src="./img/recent-photos-4.jpg" class="img-fluid" alt=""></a></div>
             <div class="swiper-slide"><a href="./img/recent-photos-5.jpg" class="glightbox"><img src="./img/recent-photos-5.jpg" class="img-fluid" alt=""></a></div>
             <div class="swiper-slide"><a href="./img/recent-photos-6.jpg" class="glightbox"><img src="./img/recent-photos-6.jpg" class="img-fluid" alt=""></a></div>
             <div class="swiper-slide"><a href="./img/recent-photos-7.jpg" class="glightbox"><img src="./img/recent-photos-7.jpg" class="img-fluid" alt=""></a></div>
-            <div class="swiper-slide"><a href="./img/recent-photos-8.jpg" class="glightbox"><img src="./img/recent-photos-8.jpg" class="img-fluid" alt=""></a></div>
+            <div class="swiper-slide"><a href="./img/recent-photos-8.jpg" class="glightbox"><img src="./img/recent-photos-8.jpg" class="img-fluid" alt=""></a></div> -->
           </div>
           <div class="swiper-pagination"></div>
         </div>
@@ -272,15 +297,15 @@ if(isset($_GET['error'])){
   <!-- ======= Footer ======= -->
   <footer id="footer">
     <div class="container">
-      <h3>MeFamily</h3>
-      <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
-      <div class="social-links">
+      <h3><?=$slogan['text'];?></h3>
+      <p>是製造人才的搖籃還是變身社畜前的蛹？</p>
+      <!-- <div class="social-links">
         <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
         <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
         <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
         <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
         <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-      </div>
+      </div> -->
       <div class="copyright">
         &copy; Copyright <strong><span>MeFamily</span></strong>. All Rights Reserved
       </div>
@@ -289,7 +314,7 @@ if(isset($_GET['error'])){
         <!-- You can delete the links only if you purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/family-multipurpose-html-bootstrap-template-free/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        Designed by <a href="https://bootstrapmade.com/">Julie Chen. Yeah, it's moi</a>
       </div>
     </div>
   </footer><!-- End Footer -->
