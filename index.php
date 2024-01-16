55a28c077b7a8085bad6506fea8a7a46527a0ba9
s8140156 <s8140156@gmail.com>
2024-01-16 22:38:40 +0800

    調整最新消息hover樣式及增加自製banner*3

--- index.php
+++ index.php
@@ -201,7 +201,7 @@ if(isset($_GET['error'])){
 		        	}
 			      ?>
             <!-- <a href="our-story.html" class="btn-learn-more">Learn More</a> -->
-		        <div id="altt" style="position: absolute; width: 380px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
+		        <div id="altt" style="position: absolute; width: 380px; min-height: 100px; background-color:#333333; color:white; top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double blue; background-position: initial initial; background-repeat: initial initial;"></div>
             
           </div>
         </div>
